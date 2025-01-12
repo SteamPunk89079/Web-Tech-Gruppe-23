5b5843d6115cf1f375ac9431524c7b6c8465a0a7
SteamPunk89079 <99643838+SteamPunk89079@users.noreply.github.com>
2025-01-12 05:02:55 +0100

    Merge progress, not final

--- Scripts/Hobbies.js
+++ Scripts/Hobbies.js
@@ -4,28 +4,30 @@ const aktivitaeten = document.getElementById("Aktivitaeten_button");
 const kalendar = document.getElementById("Kalendar_button");
 const events = document.getElementById("Events_button");
 const hobbies = document.getElementById("Hobbies_button");
+const theater = document.getElementById("Theater_button");
 
 
 
- home.addEventListener("click", function(){
-   window.location.href = "../index.html";})
 
- karte.addEventListener("click", function(){
-    window.location.href = "/pages/Karte.html";})
 
+home.addEventListener("click", function(){
+  window.location.href = "../index.html";
+});
+karte.addEventListener("click", function(){
+  window.location.href = "/pages/Karte.html";
+});
 aktivitaeten.addEventListener("click",function(){
-   window.location.href = "/pages/Aktivitäten.html";} )
-
-kalendar.addEventListener('click', function(){
+  window.location.href = "/pages/Aktivitäten.html";
+});
+kalendar.addEventListener("click", function(){
   window.location.href = "/pages/Kalendar.html";
-  }
-)
-events.addEventListener(
-  'click', function(){
-    window.location.href = "/pages/Events.html";
-  }
-)
-
+});
+events.addEventListener("click", function(){
+  window.location.href = "/pages/Events.html";
+});
 hobbies.addEventListener("click",function(){
-    window.location.href = "/pages/Hobbies.html";} )
- 
+  window.location.href = "/pages/Hobbies.html";
+});
+theater.addEventListener("click", function(){
+  window.location.href = "/pages/Theater.html";
+});
